68b61272e5f53450b230e8cf7664a15681a9cf26
mvonsteinkirch <bt3@protonmail.ch>
2023-02-05 18:22:15 -0800

    ready to add other chains

--- w3kit/src/ethereum/connections.rs
+++ w3kit/src/ethereum/connections.rs
@@ -12,9 +12,9 @@ use crate::{
 };
 
 
-pub async fn web3_connect(provider_url: &str, account_address: &str) -> web3::Result {
+pub async fn web3_connect(blockchain: &str, provider_url: &str, account_address: &str) -> web3::Result {
 
-    println!("✅ connecting to {:?}", provider_url);
+    println!("✅ connecting to {:?}", blockchain);
     let transport = web3::transports::Http::new(provider_url)?;
     get_accounts(web3::transports::Either::Right(transport), account_address).await
 
--- w3kit/src/utils/cli_handler.rs
+++ w3kit/src/utils/cli_handler.rs
@@ -10,9 +10,11 @@ use crate::{
 pub async fn handle_ws(args: ConnectionArgs) {
 
     let blockchain = &args.blockchain.to_string();
+    let url = &args.url.to_string();
+    let account = &args.account.to_string();
 
     if blockchain == "ethereum" {
-        ethereum::connections::web3_connect(&args.url.to_string(), &args.account.to_string()).await;
+        ethereum::connections::web3_connect(blockchain, url, account).await;
     } else {
         println!("❌ blockchain not supported yet");
     }
@@ -21,9 +23,11 @@ pub async fn handle_ws(args: ConnectionArgs) {
 pub async fn handle_http(args: ConnectionArgs) {
 
     let blockchain = &args.blockchain.to_string();
+    let url = &args.url.to_string();
+    let account = &args.account.to_string();
 
     if blockchain == "ethereum" {
-        ethereum::connections::web3_connect(&args.url.to_string(), &args.account.to_string()).await;
+        ethereum::connections::web3_connect(blockchain, url, account).await;
     } else {
         println!("❌ blockchain not supported yet");
     }
